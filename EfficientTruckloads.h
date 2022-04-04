e58a2117c62d3ed4224609567ab5e21f65755519
a1798069 <87979517+a1798069@users.noreply.github.com>
2022-04-04 23:53:49 +0930

    Update EfficientTruckloads.h

--- EfficientTruckloads.h
+++ EfficientTruckloads.h
@@ -2,12 +2,19 @@
 #define EFFICIENTTRUCKLOADS_H
 #include <unordered_map>
 
+using namespace std;
+
 class EfficientTruckloads {
+
 public:
-  EfficientTruckloads();
-  int numTrucks(int numCrates, int loadSize);
+    EfficientTruckloads();
+    int numTrucks(int numCrates, int loadSize);
 
 private:
-  unordered_map<int, int> hold;
-  int currentLoadSize = -1;
+    unordered_map<int,int> mem;
+    int current_loadSize = -1;
+
+
 };
+
+#endif 
